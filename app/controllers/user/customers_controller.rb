79c700112a00295d82c739fbb65efdf316bff2ea
soueiii <kumamon0316@icloud.com>
2022-04-25 03:27:19 +0000

    ユーザ編集機能

--- app/controllers/user/customers_controller.rb
+++ app/controllers/user/customers_controller.rb
@@ -5,10 +5,12 @@ class User::CustomersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_customer_path(@user.id)
   end
 
   def confirm
@@ -16,4 +18,11 @@ class User::CustomersController < ApplicationController
 
   def unsubscribe
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :self_introduction, :club, :email, :name)
+  end
 end
+
