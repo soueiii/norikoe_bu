4a7646817e761ad0ddaeb1244701c42a87b13c3e
soueiii <kumamon0316@icloud.com>
2022-05-15 09:06:29 +0000

    空欄で検索できないようにする

--- app/models/counseling.rb
+++ app/models/counseling.rb
@@ -2,7 +2,7 @@ class Counseling < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   has_many :answers, dependent: :destroy
-  validates :level, presence: true, numericality: { greater_than_or_equal_to: 1, less_than: 5 }
+  validates :level, presence: true, numericality: { greater_than_or_equal_to: 1, less_than: 6 }
   validates :title, presence: true
   validates :content, presence: true
 end
